59968444d6a46c698899b4e3b0fb14ac876413f2
mhamad raad <hamaraad883@gmail.com>
2025-08-02 02:58:45 +0300

    delete unecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { type RootState, type AppDispatch } from './store';
-import { loadOrders } from './store/OrderSlices/orderSlice';
-import ordersData from './data/mock-orders.json';
+import { useSelector } from 'react-redux';
+import { type RootState } from './store';
 
 import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
 import Layout from './components/Layout/Layout';
 import Dashboard from './pages/Dashboard';
 
 const App = () => {
-  const dispatch = useDispatch<AppDispatch>();
   const mode = useSelector((state: RootState) => state.theme.mode);
-
-  useEffect(() => {
-    dispatch(loadOrders(ordersData.orders));
-  }, [dispatch]);
 
   const theme = createTheme({
     palette: {
