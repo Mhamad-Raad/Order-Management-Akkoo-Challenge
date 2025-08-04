6364828c8fc2113ee736614b58acdb47a13208fb
mhamad raad <hamaraad883@gmail.com>
2025-08-04 03:45:16 +0300

    add enviroment varibles

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAcwSWFlVRTjxX_zIlYy-LTaQriaA3mHWs',
-  authDomain: 'order-management-724c7.firebaseapp.com',
-  projectId: 'order-management-724c7',
-  storageBucket: 'order-management-724c7.firebasestorage.app',
-  messagingSenderId: '495145564783',
-  appId: '1:495145564783:web:e2223c2c456fe10afc6b9a',
-  measurementId: 'G-R5E5Q7ZDVK',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
